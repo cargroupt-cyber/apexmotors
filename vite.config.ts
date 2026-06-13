76d3196f70d379db53550b8b457f23ca52977844
cargroupt-cyber <cargroupt@gmail.com>
2026-06-13 01:22:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'plugin-inspect-react-code'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), react()],
   build: {
     outDir: 'build',
     emptyOutDir: true,
   },
+  plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
   },
